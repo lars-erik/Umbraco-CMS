913db414e2797015421b06593fdf9e8759dcc2ec
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2018-09-02 22:01:29 +0200

    full impl from other branch

--- src/Our.Umbraco.Container.Castle/CastleContainer.cs
+++ src/Our.Umbraco.Container.Castle/CastleContainer.cs
@@ -3,102 +3,196 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Castle.Core;
+using Castle.MicroKernel.Lifestyle;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.Resolvers;
+using Castle.Windsor;
+using Castle.Windsor.Diagnostics.Helpers;
+using Castle.Windsor.Installer;
 using Umbraco.Core.Composing;
 
 namespace Our.Umbraco.Container.Castle
 {
     public class CastleContainer : IContainer
     {
-        public object ConcreteContainer { get; }
+        // fixme - creates a cyclic dependency with castle
+        public static IContainer Create()
+        {
+            var castleContainer = new CastleContainer();
+            return castleContainer;
+        }
+
+        private Dictionary<Lifetime, LifestyleType> lifetimes = new Dictionary<Lifetime, LifestyleType>
+        {
+            { Lifetime.Request, LifestyleType.PerWebRequest },
+            { Lifetime.Scope, LifestyleType.Scoped },
+            { Lifetime.Singleton, LifestyleType.Singleton },
+            { Lifetime.Transient, LifestyleType.Transient },
+        };
+
+        private WindsorContainer container;
+
+        public object ConcreteContainer => container;
+
+        public CastleContainer()
+        {
+            container = new WindsorContainer();
+            container.Register(Component.For<IContainer>().Instance(this));
+            container.Register(Component.For<ILazyComponentLoader>().ImplementedBy<LazyOfTComponentLoader>());
+
+        }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            container.Dispose();
         }
 
         public object GetInstance(Type type)
         {
-            throw new NotImplementedException();
+            return container.Resolve(type);
         }
 
         public object GetInstance(Type type, string name)
         {
-            throw new NotImplementedException();
+            return container.Resolve(name, type);
         }
 
+        // fixme - figure out what castle actually does vs. xml doc
         public object TryGetInstance(Type type)
         {
-            throw new NotImplementedException();
+            return container.Resolve(type);
         }
 
         public IEnumerable<object> GetAllInstances(Type serviceType)
         {
-            throw new NotImplementedException();
+            return container.ResolveAll(serviceType).Cast<object>();
         }
 
         public IEnumerable<TService> GetAllInstances<TService>()
         {
-            throw new NotImplementedException();
+            return container.ResolveAll<TService>();
         }
 
+        // fixme - Translate to Registration object
         public IEnumerable<Registration> GetRegistered(Type serviceType)
         {
-            throw new NotImplementedException();
+            return container.Kernel.GetAssignableHandlers(serviceType)
+                .Select(x => new Registration(serviceType, x.GetComponentName())
+            );
         }
 
-        public object CreateInstance(Type type, params object[] args)
+        // fixme - Refactor to use Dictionary. Castle does not support nameless parameters. !?!?
+        public object CreateInstance(Type type, IDictionary<string, object> args)
         {
-            throw new NotImplementedException();
+            return container.Resolve(type, args);
         }
 
         public void Register(Type serviceType, Lifetime lifetime = Lifetime.Transient)
         {
-            throw new NotImplementedException();
+            container.Register(
+                Component
+                    .For(serviceType)
+                    .LifeStyle.Is(lifetimes[lifetime])
+            );
         }
 
         public void Register(Type serviceType, Type implementingType, Lifetime lifetime = Lifetime.Transient)
         {
-            throw new NotImplementedException();
+            container.Register(
+                Component
+                    .For(serviceType)
+                    .ImplementedBy(implementingType)
+                    .LifeStyle.Is(lifetimes[lifetime])
+            );
         }
 
         public void Register(Type serviceType, Type implementingType, string name, Lifetime lifetime = Lifetime.Transient)
         {
-            throw new NotImplementedException();
+            container.Register(
+                Component
+                    .For(serviceType)
+                    .Named(name)
+                    .ImplementedBy(implementingType)
+                    .LifeStyle.Is(lifetimes[lifetime])
+            );
         }
 
+        // fixme - Must make TService reference type in interface
+        // fixme - Umbraco re-registers IProfiler for instance. Must set new registrations as default. (?)
         public void Register<TService>(Func<IContainer, TService> factory, Lifetime lifetime = Lifetime.Transient)
         {
-            throw new NotImplementedException();
+            container.Register(
+                Component
+                    .For(typeof(TService))
+                    .UsingFactory<IContainer, TService>(f => factory(this))
+                    .LifeStyle.Is(lifetimes[lifetime])
+                    .IsDefault()
+            );
+
+        }
+
+        // fixme - Must make TService reference type in interface
+        // fixme - Umbraco re-registers IProfiler for instance. Must set new registrations as default. (?)
+        public void Register<TService>(Func<IContainer, TService> factory, string name, Lifetime lifetime = Lifetime.Transient)
+        {
+            container.Register(
+                Component
+                    .For(typeof(TService))
+                    .UsingFactory<IContainer, TService>(f => factory(this))
+                    .Named(name)
+                    .LifeStyle.Is(lifetimes[lifetime])
+                    .IsDefault()
+            );
+
         }
 
         public void RegisterInstance(Type serviceType, object instance)
         {
-            throw new NotImplementedException();
+            container.Register(
+                Component
+                    .For(serviceType)
+                    .Instance(instance));
         }
 
         public void RegisterAuto(Type serviceBaseType)
         {
-            throw new NotImplementedException();
+            container.Register(
+                Classes
+                .FromAssemblyContaining(serviceBaseType)
+                .BasedOn(serviceBaseType)
+            );
         }
 
+        // fixme Figure out if Castle returns things in same order as registered
         public void RegisterOrdered(Type serviceType, Type[] implementingTypes, Lifetime lifetime = Lifetime.Transient)
         {
-            throw new NotImplementedException();
+            foreach(var type in implementingTypes)
+            { 
+                container.Register(
+                    Component
+                        .For(serviceType)
+                        .ImplementedBy(type)
+                        .LifeStyle.Is(lifetimes[lifetime])
+                );
+            }
         }
 
         public IDisposable BeginScope()
         {
-            throw new NotImplementedException();
+            return container.BeginScope();
         }
 
+        // fixme - Figure out requirements
         public IContainer ConfigureForWeb()
         {
-            throw new NotImplementedException();
+            return this;
         }
 
+        // fixme - Figure out if this is on by default and how to set it.
         public IContainer EnablePerWebRequestScope()
         {
-            throw new NotImplementedException();
+            return this;
         }
     }
 }
