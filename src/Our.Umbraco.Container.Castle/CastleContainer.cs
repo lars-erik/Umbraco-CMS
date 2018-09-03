8c73e68de64c17e682a5ff405db82d24aa0e5fa6
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2018-09-03 22:48:21 +0200

    Banging head against controller factory

--- src/Our.Umbraco.Container.Castle/CastleContainer.cs
+++ src/Our.Umbraco.Container.Castle/CastleContainer.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 using Castle.Core;
+using Castle.MicroKernel;
 using Castle.MicroKernel.Lifestyle;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.Resolvers;
@@ -12,6 +16,7 @@ using Castle.Windsor;
 using Castle.Windsor.Diagnostics.Helpers;
 using Castle.Windsor.Installer;
 using Umbraco.Core.Composing;
+using IDependencyResolver = System.Web.Mvc.IDependencyResolver;
 
 namespace Our.Umbraco.Container.Castle
 {
@@ -187,6 +192,8 @@ namespace Our.Umbraco.Container.Castle
         // fixme - Figure out requirements
         public IContainer ConfigureForWeb()
         {
+            DependencyResolver.SetResolver(new WindsorDependencyResolver(container));
+
             return this;
         }
 
@@ -194,6 +201,53 @@ namespace Our.Umbraco.Container.Castle
         public IContainer EnablePerWebRequestScope()
         {
             return this;
+        }
+    }
+
+    public class WindsorDependencyResolver : IDependencyResolver
+    {
+        private readonly WindsorContainer container;
+
+        public WindsorDependencyResolver(WindsorContainer container)
+        {
+            this.container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return container.Resolve(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return container.ResolveAll(serviceType).Cast<object>();
+        }
+    }
+
+    /// <summary>
+    /// Possibly not needed at all?
+    /// </summary>
+    public class WindsorControllerFactory : DefaultControllerFactory
+    {
+        private readonly IContainer container;
+
+        public WindsorControllerFactory(IContainer container)
+        {
+            this.container = container;
+        }
+
+        public override void ReleaseController(IController controller)
+        {
+            ((WindsorContainer)container.ConcreteContainer).Kernel.ReleaseComponent(controller);
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType == null)
+            {
+                throw new HttpException(404, string.Format("The controller for path '{0}' could not be found.", requestContext.HttpContext.Request.Path));
+            }
+            return (IController)container.GetInstance(controllerType);
         }
     }
 }
