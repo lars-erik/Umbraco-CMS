0ea7d81be3ab936c20dfd46fd1a17ec2fc50e19f
Lars-Erik Aabech <lea@markedspartner.no>
2018-09-09 20:06:05 +0200

    NPocoSqlTests with Castle. Side effect shows that mappercollectionbuilder is incompatible with Castle.

---
+++ src/Our.Umbraco.Container.Castle.Tests/NPocoSqlTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Umbraco.Core.Composing;
+using Current = Umbraco.Web.Composing.Current;
+
+namespace Our.Umbraco.Container.Castle.Tests
+{
+    [TestFixture]
+    public class NPocoSqlTests : global::Umbraco.Tests.Persistence.NPocoTests.NPocoSqlTests
+    {
+        public override void PreSetUp()
+        {
+            Current.Container.RegisterInstance(Current.Container);
+        }
+
+    }
+}
